8f7ee360906c5f700699127a9715d60c8db3a3b9
charlotteassemat <charlotteassemat33@gmail.com>
2015-04-12 15:43:40 +0200

    *Adding skel_model to double check we use the right skeleton.*This version will fail if there is NaN in prob matrix in the end; it doesnt with me

--- fit_linear_data.m
+++
@@ -1,8 +0,0 @@
-function [betas sigma] = fit_linear_gaussian(Y,X)
-%
-%  Input:
-%     Y: vector Dx1 with the observations for the variable
-%     X: matrix DxV with the observations for the parent variables
-%               of X. V is the number of parent variables
-%
-
